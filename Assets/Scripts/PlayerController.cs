6d333b38153130b2e1a7aa5185f06623b41dfc00
tommytech2 <tommy.dockrill242@gmail.com>
2026-04-22 10:33:53 +1000

    WIP: Pickups added for extra value and features completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 13)
+        if(count >= 39)
         {
             winTextObject.SetActive(true);
         
